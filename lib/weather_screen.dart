86a924f1bdf1dfcb1e06f562a66c3de71287fc97
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-08-11 15:03:10 +0530

    this is ui of weather app learning

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,1 +1,142 @@
+import 'dart:ui';
+import 'package:weather/additional_info_item.dart';
+
+import 'package:weather/hourly_forecaste_item.dart';
+
 import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              '300 °K',
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Icon(
+                              Icons.cloud,
+                              size: 62,
+                            ),
+                            Text(
+                              'Rain ',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Weather Forecast',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 21,
+            ),
+            const SizedBox(height: 16),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForcasteItem(
+                    time: '00:00',
+                    icon: Icons.cloud,
+                    temprature: '301.22',
+                  ),
+                  HourlyForcasteItem(
+                    time: '12:00',
+                    icon: Icons.cloud,
+                    temprature: '301.22',
+                  ),
+                  HourlyForcasteItem(
+                    time: '15:00',
+                    icon: Icons.sunny,
+                    temprature: '301.22',
+                  ),
+                  HourlyForcasteItem(
+                    time: '18:00',
+                    icon: Icons.sunny_snowing,
+                    temprature: '301.22',
+                  ),
+                  HourlyForcasteItem(
+                    time: '21:00',
+                    icon: Icons.cloud,
+                    temprature: '301.22',
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            const Text(
+              'Additional Information',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                AdditionalInfoItem(
+                  icon: Icons.water_drop,
+                  label: 'Humidity',
+                  value: '91%',
+                ),
+                AdditionalInfoItem(
+                    icon: Icons.air, label: 'Wind Speed', value: '7.5 kmph'),
+                AdditionalInfoItem(
+                    icon: Icons.beach_access,
+                    label: 'Pressure',
+                    value: '1001 mb'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
